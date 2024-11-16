6fe1bde5f6d6ba8be9ef1bce2083f5cdd105986a
Daniel Pieper <daniel@daniel-pieper.com>
2024-11-16 19:57:23 +0100

    Fix tailscale exit node dns issue

--- modules/nixos/services/tailscale/default.nix
+++ modules/nixos/services/tailscale/default.nix
@@ -14,5 +14,8 @@ in
 
   config = mkIf cfg.enable {
     services.tailscale.enable = true;
+
+    # https://github.com/tailscale/tailscale/issues/10319#issuecomment-1886730614
+    networking.firewall.checkReversePath = "loose";
   };
 }
