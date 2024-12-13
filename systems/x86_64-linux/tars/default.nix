62955052872eaf66d249424fefa0bb548b6918c9
Daniel Pieper <daniel@daniel-pieper.com>
2024-12-13 07:31:44 +0100

    Add tars

---
+++ systems/x86_64-linux/tars/default.nix
@@ -0,0 +1,29 @@
+{
+  pkgs,
+  lib,
+  ...
+}:
+{
+  imports = [
+    ./hardware-configuration.nix
+    ./disks.nix
+  ];
+
+  system.impermanence.enable = true;
+
+  roles = {
+    desktop.enable = true;
+  };
+
+  topology.self = {
+    hardware.info = "Lenovo Thinkpad x250";
+  };
+
+  boot = {
+    supportedFilesystems = lib.mkForce [ "btrfs" ];
+    kernelPackages = pkgs.linuxPackages_latest;
+    resumeDevice = "/dev/disk/by-label/nixos";
+  };
+
+  system.stateVersion = "23.11";
+}
