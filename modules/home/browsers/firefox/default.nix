1df1d07f952775100a8fe18713c926f6f6863d9b
Daniel Pieper <git@daniel-pieper.com>
2024-12-19 17:44:37 +0100

    Fix firefox userChrome

--- modules/home/browsers/firefox/default.nix
+++ modules/home/browsers/firefox/default.nix
@@ -207,6 +207,50 @@ in
         userChrome = ''
           @import "firefox-gnome-theme/userChrome.css";
 
+          /* show bookmark toolbar in fullscreen */
+          :root[inFullscreen] #PersonalToolbar {
+            visibility: visible !important;
+          }
+
+
+          /* Decrease size of the sidebar header */
+          #sidebar-header {
+            font-size: 1.2em !important;
+            padding: 2px 6px 2px 3px !important;
+          }
+          #sidebar-header #sidebar-close {
+            padding: 3px !important;
+          }
+          #sidebar-header #sidebar-close .toolbarbutton-icon {
+            width: 14px !important;
+            height: 14px !important;
+            opacity: 0.6 !important;
+          }
+
+
+          /**
+          * Dynamic Horizontal Tabs Toolbar (with animations)
+          * sidebar.verticalTabs: false (with native horizontal tabs)
+          */
+          #main-window #TabsToolbar > .toolbar-items {
+            overflow: hidden;
+            transition: height 0.3s 0.3s !important;
+          }
+          /* Default state: Set initial height to enable animation */
+          #main-window #TabsToolbar > .toolbar-items { height: 3em !important; }
+          #main-window[uidensity="touch"] #TabsToolbar > .toolbar-items { height: 3.35em !important; }
+          #main-window[uidensity="compact"] #TabsToolbar > .toolbar-items { height: 2.7em !important; }
+          /* Hidden state: Hide native tabs strip */
+          #main-window[titlepreface*="XXX"] #TabsToolbar > .toolbar-items { height: 0 !important; }
+          /* Hidden state: Fix z-index of active pinned tabs */
+          #main-window[titlepreface*="XXX"] #tabbrowser-tabs { z-index: 0 !important; }
+          /* Hidden state: Hide window buttons in tabs-toolbar */
+          #main-window[titlepreface*="XXX"] #TabsToolbar .titlebar-spacer,
+          #main-window[titlepreface*="XXX"] #TabsToolbar .titlebar-buttonbox-container {
+            display: none !important;
+          }
+
+
           :root {{
           --gnome-browser-before-load-background:        rgb(30, 30, 46)};
           --gnome-accent-bg:                             rgb(137, 180, 250);
@@ -288,53 +332,6 @@ in
           --trailhead-card-button-background-color: rgba(12, 12, 13, 0.3)!important;
           --trailhead-card-button-background-hover-color: rgba(12, 12, 13, 0.5)!important;
           --trailhead-card-button-background-active-color: rgba(12, 12, 13, 0.7)!important;
-
-        /* show bookmark toolbar in fullscreen */
-        :root[inFullscreen] #PersonalToolbar {
-          visibility: visible !important;
-        }
-
-        /* hide native tabs if sideberry is enabled */
-        /*
-          Set window preface value:
-          Sidebery settings > Help > Preface value
-          note: in this example: XXX
-        */
-        #main-window #TabsToolbar {
-            overflow: hidden;
-            max-height: 3em; /* Estado expandido */
-            transition: max-height 0.3s ease-in-out !important;
-        }
-        #main-window #TabsToolbar .toolbar-items {
-            overflow: hidden;
-            max-height: 40px; /* Estado expandido */
-            transition: max-height 0.3s ease-in-out !important;
-        }
-        #main-window[titlepreface*="XXX"] #TabsToolbar {
-            max-height: 0; /* Estado colapsado */
-            visibility: hidden; /* Evita conteúdo interativo enquanto colapsado */
-            transition: max-height 0.3s ease-in-out, visibility 0s 0.3s !important; /* atraso para ocultar */
-        }
-        #main-window[titlepreface*="XXX"] #TabsToolbar .toolbar-items {
-            max-height: 0;
-        }
-        #main-window[titlepreface*="XXX"] #tabbrowser-tabs {
-            z-index: 0 !important;
-        }
-
-        /* Decrease size of the sidebar header */
-        #sidebar-header {
-          font-size: 1.2em !important;
-          padding: 2px 6px 2px 3px !important;
-        }
-        #sidebar-header #sidebar-close {
-          padding: 3px !important;
-        }
-        #sidebar-header #sidebar-close .toolbarbutton-icon {
-          width: 14px !important;
-          height: 14px !important;
-          opacity: 0.6 !important;
-        }
         '';
 
         userContent = ''
