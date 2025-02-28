27df553a45e78ca4456b14eac4ea58d8060693ba
Daniel Pieper <daniel@daniel-pieper.com>
2025-02-28 17:25:31 +0100

    Add date/time to hyprlock

--- modules/home/desktops/addons/hyprlock/default.nix
+++ modules/home/desktops/addons/hyprlock/default.nix
@@ -20,6 +20,32 @@ in
         auth = {
           fingerprint.enabled = true;
         };
+        background = {
+          path = lib.mkForce "screenshot";
+          blur_passes = 2;
+          blur_size = 4;
+          noise = 0.011700;
+        };
+        label = [
+          {
+            text = "$TIME";
+            color = "$text";
+            font_size = 90;
+            font_family = "$font";
+            position = "-30, 0";
+            halign = "right";
+            valign = "top";
+          }
+          {
+            text = "cmd[update:43200000] date +\"%A, %d %B %Y\"";
+            color = "$text";
+            font_size = 25;
+            font_family = "$font";
+            position = "-30, -150";
+            halign = "right";
+            valign = "top";
+          }
+        ];
       };
     };
   };
