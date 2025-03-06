d0ce5544d98edb9b6c0f388cf67ab614fe3521f8
Daniel Pieper <daniel@daniel-pieper.com>
2025-03-06 15:15:41 +0100

    Add german spellcheck to neovim

--- modules/home/cli/editors/nvim/default.nix
+++ modules/home/cli/editors/nvim/default.nix
@@ -87,8 +87,34 @@ in
       };
     };
 
-    xdg.configFile."nvim/lua" = {
-      source = "${inputs.lazyvim}/lua";
-    };
+    xdg.configFile =
+      let
+        nvim-spell-de-utf8-dictionary = builtins.fetchurl {
+          url = "https://www.mirrorservice.org/pub/vim/runtime/spell/de.utf-8.spl";
+          sha256 = "1ld3hgv1kpdrl4fjc1wwxgk4v74k8lmbkpi1x7dnr19rldz11ivk";
+        };
+
+        nvim-spell-de-utf8-suggestions = builtins.fetchurl {
+          url = "https://www.mirrorservice.org/pub/vim/runtime/spell/de.utf-8.sug";
+          sha256 = "0j592ibsias7prm1r3dsz7la04ss5bmsba6l1kv9xn3353wyrl0k";
+        };
+
+        nvim-spell-de-latin1-dictionary = builtins.fetchurl {
+          url = "https://www.mirrorservice.org/pub/vim/runtime/spell/de.latin1.spl";
+          sha256 = "0hn303snzwmzf6fabfk777cgnpqdvqs4p6py6jjm58hdqgwm9rw9";
+        };
+
+        nvim-spell-de-latin1-suggestions = builtins.fetchurl {
+          url = "https://www.mirrorservice.org/pub/vim/runtime/spell/de.latin1.sug";
+          sha256 = "0mz07d0a68fhxl9vmy1548vnbayvwv1pc24zhva9klgi84gssgwm";
+        };
+      in
+      {
+        "nvim/lua".source = "${inputs.lazyvim}/lua";
+        "nvim/spell/de.utf-8.spl".source = nvim-spell-de-utf8-dictionary;
+        "nvim/spell/de.utf-8.sug".source = nvim-spell-de-utf8-suggestions;
+        "nvim/spell/de.latin1.spl".source = nvim-spell-de-latin1-dictionary;
+        "nvim/spell/de.latin1.sug".source = nvim-spell-de-latin1-suggestions;
+      };
   };
 }
