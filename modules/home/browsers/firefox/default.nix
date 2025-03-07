51ee9d01f10fa882e181b1a7327483d3cab4584a
Daniel Pieper <daniel@daniel-pieper.com>
2025-03-07 10:43:16 +0100

    Remove obsolete styling from firefox
    With vertical tabs and (beta) tab groups now available,
    the sideberry addon and its styling is no longer needed.

--- modules/home/browsers/firefox/default.nix
+++ modules/home/browsers/firefox/default.nix
@@ -13,21 +13,18 @@ let
   mkFirefoxProfile = profileName: profileId: {
     name = profileName;
     id = profileId;
-    extraConfig = ''
-      ${builtins.readFile "${inputs.firefox-gnome-theme}/configuration/user.js"}
-    '';
 
     extensions.packages = with pkgs.nur.repos.rycee.firefox-addons; [
       onepassword-password-manager
+      ublock-origin
     ];
 
     settings = {
-      "browser.uidensity" = 0;
-      "gnomeTheme.activeTabContrast" = true;
       "gnomeTheme.hideSingleTab" = false;
+      "gnomeTheme.activeTabContrast" = true;
       "gnomeTheme.hideWebrtcIndicator" = true;
       "gnomeTheme.systemIcons" = true;
-      "gnomeTheme.spinner" = true;
+      "gnomeTheme.bookmarksOnFullscreen" = true;
       "layers.acceleration.force-enabled" = true;
       "identity.fxaccounts.account.device.name" = "${config.flocke.user.name}@${host}";
       "browser.urlbar.oneOffSearches" = false;
@@ -41,139 +38,9 @@ let
       "browser.urlbar.suggest.topsites" = false;
     };
 
-    userChrome = ''
-      @import "firefox-gnome-theme/userChrome.css";
-
-      /* show bookmark toolbar in fullscreen */
-      :root[inFullscreen] #PersonalToolbar {
-        visibility: visible !important;
-      }
-
-
-      /* Decrease size of the sidebar header */
-      #sidebar-header {
-        font-size: 1.2em !important;
-        padding: 2px 6px 2px 3px !important;
-      }
-      #sidebar-header #sidebar-close {
-        padding: 3px !important;
-      }
-      #sidebar-header #sidebar-close .toolbarbutton-icon {
-        width: 14px !important;
-        height: 14px !important;
-        opacity: 0.6 !important;
-      }
-
-
-      /**
-      * Dynamic Horizontal Tabs Toolbar (with animations)
-      * sidebar.verticalTabs: false (with native horizontal tabs)
-      */
-      #main-window #TabsToolbar > .toolbar-items {
-        overflow: hidden;
-        transition: height 0.3s 0.3s !important;
-      }
-      /* Default state: Set initial height to enable animation */
-      #main-window #TabsToolbar > .toolbar-items { height: 3em !important; }
-      #main-window[uidensity="touch"] #TabsToolbar > .toolbar-items { height: 3.35em !important; }
-      #main-window[uidensity="compact"] #TabsToolbar > .toolbar-items { height: 2.7em !important; }
-      /* Hidden state: Hide native tabs strip */
-      #main-window[titlepreface*="XXX"] #TabsToolbar > .toolbar-items { height: 0 !important; }
-      /* Hidden state: Fix z-index of active pinned tabs */
-      #main-window[titlepreface*="XXX"] #tabbrowser-tabs { z-index: 0 !important; }
-      /* Hidden state: Hide window buttons in tabs-toolbar */
-      #main-window[titlepreface*="XXX"] #TabsToolbar .titlebar-spacer,
-      #main-window[titlepreface*="XXX"] #TabsToolbar .titlebar-buttonbox-container {
-        display: none !important;
-      }
-
-
-      :root {{
-      --gnome-browser-before-load-background:        rgb(30, 30, 46)};
-      --gnome-accent-bg:                             rgb(137, 180, 250);
-      --gnome-accent:                                rgb(116, 199, 236);
-      --gnome-toolbar-background:                    rgb(30, 30, 46);
-      --gnome-toolbar-color:                         rgb(205, 214, 244);
-      --gnome-toolbar-icon-fill:                     rgb(205, 214, 244);
-      --gnome-inactive-toolbar-color:                rgb(30, 30, 46);
-      --gnome-inactive-toolbar-border-color:         rgb(49, 50, 68);
-      --gnome-inactive-toolbar-icon-fill:            rgb(205, 214, 244);
-      --gnome-menu-background:                       rgb(24, 24, 37);
-      --gnome-headerbar-background:                  rgb(17, 17, 27);
-      --gnome-button-destructive-action-background:  rgb(237, 135, 150);
-      --gnome-entry-color:                           rgb(205, 214, 244);
-      --gnome-inactive-entry-color:                  rgb(205, 214, 244);
-      --gnome-switch-slider-background:              rgb(24, 24, 37);
-      --gnome-switch-active-slider-background:       rgb(116, 199, 236);
-      --gnome-inactive-tabbar-tab-background:        rgb(30, 30, 46);
-      --gnome-inactive-tabbar-tab-active-background: rgba(255,255,255,0.025);
-      --gnome-tabbar-tab-background:                 rgb(30, 30, 46);
-      --gnome-tabbar-tab-hover-background:           rgba(255,255,255,0.025);
-      --gnome-tabbar-tab-active-background:          rgba(255,255,255,0.075);
-      --gnome-tabbar-tab-active-hover-background:    rgba(255,255,255,0.100);
-      --gnome-tabbar-tab-active-background-contrast: rgba(255,255,255,0.125);
-      }}
-
-      @-moz-document url-prefix(about:home), url-prefix(about:newtab) {{
-      body{{
-      --newtab-background-color: #2A2A2E!important;
-      --newtab-border-primary-color: rgba(249, 249, 250, 0.8)!important;
-      --newtab-border-secondary-color: rgba(249, 249, 250, 0.1)!important;
-      --newtab-button-primary-color: #0060DF!important;
-      --newtab-button-secondary-color: #38383D!important;
-      --newtab-element-active-color: rgba(249, 249, 250, 0.2)!important;
-      --newtab-element-hover-color: rgba(249, 249, 250, 0.1)!important;
-      --newtab-icon-primary-color: rgba(249, 249, 250, 0.8)!important;
-      --newtab-icon-secondary-color: rgba(249, 249, 250, 0.4)!important;
-      --newtab-icon-tertiary-color: rgba(249, 249, 250, 0.4)!important;
-      --newtab-inner-box-shadow-color: rgba(249, 249, 250, 0.2)!important;
-      --newtab-link-primary-color: var(--gnome-accent)!important;
-      --newtab-link-secondary-color: #50BCB6!important;
-      --newtab-text-conditional-color: #F9F9FA!important;
-      --newtab-text-primary-color: var(--gnome-accent)!important;
-      --newtab-text-secondary-color: rgba(249, 249, 250, 0.8)!important;
-      --newtab-textbox-background-color: var(--gnome-toolbar-background)!important;
-      --newtab-textbox-border: var(--gnome-inactive-toolbar-border-color)!important;
-      --newtab-textbox-focus-color: #45A1FF!important;
-      --newtab-textbox-focus-boxshadow: 0 0 0 1px #45A1FF, 0 0 0 4px rgba(69, 161, 255, 0.3)!important;
-      --newtab-feed-button-background: #38383D!important;
-      --newtab-feed-button-text: #F9F9FA!important;
-      --newtab-feed-button-background-faded: rgba(56, 56, 61, 0.6)!important;
-      --newtab-feed-button-text-faded: rgba(249, 249, 250, 0)!important;
-      --newtab-feed-button-spinner: #D7D7DB!important;
-      --newtab-contextmenu-background-color: #4A4A4F!important;
-      --newtab-contextmenu-button-color: #2A2A2E!important;
-      --newtab-modal-color: #2A2A2E!important;
-      --newtab-overlay-color: rgba(12, 12, 13, 0.8)!important;
-      --newtab-section-header-text-color: rgba(249, 249, 250, 0.8)!important;
-      --newtab-section-navigation-text-color: rgba(249, 249, 250, 0.8)!important;
-      --newtab-section-active-contextmenu-color: #FFF!important;
-      --newtab-search-border-color: rgba(249, 249, 250, 0.2)!important;
-      --newtab-search-dropdown-color: #38383D!important;
-      --newtab-search-dropdown-header-color: #4A4A4F!important;
-      --newtab-search-header-background-color: rgba(42, 42, 46, 0.95)!important;
-      --newtab-search-icon-color: rgba(249, 249, 250, 0.6)!important;
-      --newtab-search-wordmark-color: #FFF!important;
-      --newtab-topsites-background-color: #38383D!important;
-      --newtab-topsites-icon-shadow: none!important;
-      --newtab-topsites-label-color: rgba(249, 249, 250, 0.8)!important;
-      --newtab-card-active-outline-color: var(--gnome-toolbar-icon-fill)!important;
-      --newtab-card-background-color: var(--gnome-toolbar-background)!important;
-      --newtab-card-hairline-color: rgba(249, 249, 250, 0.1)!important;
-      --newtab-card-placeholder-color: #4A4A4F!important;
-      --newtab-card-shadow: 0 1px 8px 0 rgba(12, 12, 13, 0.2)!important;
-      --newtab-snippets-background-color: #38383D!important;
-      --newtab-snippets-hairline-color: rgba(255, 255, 255, 0.1)!important;
-      --trailhead-header-text-color: rgba(255, 255, 255, 0.6)!important;
-      --trailhead-cards-background-color: rgba(12, 12, 13, 0.1)!important;
-      --trailhead-card-button-background-color: rgba(12, 12, 13, 0.3)!important;
-      --trailhead-card-button-background-hover-color: rgba(12, 12, 13, 0.5)!important;
-      --trailhead-card-button-background-active-color: rgba(12, 12, 13, 0.7)!important;
-    '';
-
-    userContent = ''
-      @import "firefox-gnome-theme/userContent.css;
-    '';
+    userChrome = builtins.readFile "${inputs.firefox-gnome-theme}/userChrome.css";
+    userContent = builtins.readFile "${inputs.firefox-gnome-theme}/userContent.css";
+    extraConfig = builtins.readFile "${inputs.firefox-gnome-theme}/configuration/user.js";
   };
 in
 {
@@ -207,8 +74,6 @@ in
   };
 
   config = mkIf cfg.enable {
-    home.file.".mozilla/firefox/default/chrome/firefox-gnome-theme".source = inputs.firefox-gnome-theme;
-
     xdg.mimeApps.defaultApplications = {
       "text/html" = [ "firefox.desktop" ];
       "text/xml" = [ "firefox.desktop" ];
@@ -218,6 +83,10 @@ in
 
     programs.firefox = {
       enable = true;
+      languagePacks = [
+        "en-US"
+        "de"
+      ];
       profiles =
         lib.foldl'
           (
@@ -232,6 +101,10 @@ in
           }
           cfg.additionalProfiles;
     };
+
+    stylix.targets.firefox.profileNames = [
+      "default"
+    ] ++ (map (profile: lib.toLower profile.name) cfg.additionalProfiles);
 
     xdg.desktopEntries = lib.foldl' (
       acc: profile:
