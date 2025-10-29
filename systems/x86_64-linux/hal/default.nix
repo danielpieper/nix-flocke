4d4372e445e8116f8bf204e36b69ee461a494953
Daniel Pieper <daniel@daniel-pieper.com>
2025-10-29 20:29:30 +0100

    Disable hal services bc synology drive failure

--- systems/x86_64-linux/hal/default.nix
+++ systems/x86_64-linux/hal/default.nix
@@ -23,25 +23,25 @@
   services.flocke = {
     traefik.enable = true;
     authentik.enable = true;
-    nfs.enable = true;
-    jellyfin.enable = true;
-    navidrome.enable = true;
+    # nfs.enable = true;
+    # jellyfin.enable = true;
+    # navidrome.enable = true;
     teslamate.enable = true;
     postgresql.enable = true;
     monitoring.enable = true;
     gotify.enable = true;
-    restic = {
-      enable = true;
-      enable_server = true;
-    };
-    syncthing.enable = true;
+    # restic = {
+    #   enable = true;
+    #   enable_server = true;
+    # };
+    # syncthing.enable = true;
     forgejo = {
       enable = true;
-      enable-runner = true;
-      enable-dump = true;
+      # enable-runner = true;
+      # enable-dump = true;
     };
-    arr.enable = true;
-    home-assistant.enable = true;
+    # arr.enable = true;
+    # home-assistant.enable = true;
     avahi.enable = true;
   };
 
