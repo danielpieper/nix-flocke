7cc5ec1ea3f27072abee5a89fabe50c11d5c1f55
Daniel Pieper <daniel@daniel-pieper.com>
2026-03-30 12:12:53 +0200

    Strip migrated services from hal
    Remove authentik, forgejo, teslamate, monitoring, and gotify from hal.
    These services are now running on jarvis. Add node exporter for
    remote monitoring from jarvis Prometheus.

--- systems/x86_64-linux/hal/default.nix
+++ systems/x86_64-linux/hal/default.nix
@@ -19,27 +19,16 @@
 
   services.flocke = {
     traefik.enable = true;
-    authentik.enable = true;
     nfs.enable = true;
     jellyfin.enable = true;
     navidrome.enable = true;
-    teslamate = {
-      enable = true;
-      runMigrations = false;
-    };
     postgresql.enable = true;
-    monitoring.enable = true;
-    gotify.enable = true;
+    monitoring.enable_exporter = true;
     restic = {
       enable = true;
       enable_server = true;
     };
     syncthing.enable = true;
-    forgejo = {
-      enable = true;
-      enable-runner = false;
-      enable-dump = true;
-    };
     arr.enable = false;
     avahi.enable = true;
     evitts.enable = false;
