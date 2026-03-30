bb7400f7f5a3deca60b76ce7dc925ed44b5018d2
Daniel Pieper <daniel@daniel-pieper.com>
2026-03-30 17:09:54 +0200

    Switch ava website to Caddy, drop nginx
    Replace nginx+Traefik with Caddy for the public website on ava.
    Caddy handles TLS automatically via HTTP-01 challenge.
    Generate ava restic SSH key for Storage Box backup.

--- modules/nixos/services/website/default.nix
+++ modules/nixos/services/website/default.nix
@@ -8,42 +8,25 @@ with lib;
 with lib.flocke;
 let
   cfg = config.services.flocke.website;
+  inherit (inputs.nix-secrets) domain;
 in
 {
   options.services.flocke.website = {
-    enable = mkEnableOption "Enable ${inputs.nix-secrets.domain} hosting";
+    enable = mkEnableOption "Enable ${domain} hosting";
   };
 
   config = mkIf cfg.enable {
+    services.caddy.virtualHosts = {
+      "${domain}" = {
+        extraConfig = ''
+          root * /var/lib/${domain}
+          file_server
 
-    services = {
-      nginx = {
-        enable = true;
-        defaultListen = [
-          {
-            addr = "127.0.0.1";
-            port = 8099;
-          }
-        ];
-        virtualHosts = {
-          "${inputs.nix-secrets.domain}" = {
-            forceSSL = false;
-            enableACME = false;
-            serverAliases = [
-              "www.${inputs.nix-secrets.domain}"
-            ];
-            locations."/" = {
-              root = "/var/lib/${inputs.nix-secrets.domain}";
-            };
-          };
-        };
-        recommendedTlsSettings = true;
-        recommendedProxySettings = true;
-        recommendedGzipSettings = true;
-        recommendedOptimisation = true;
-        clientMaxBodySize = "300m";
+        '';
       };
-
+      "www.${domain}" = {
+        extraConfig = "redir https://${domain}{uri} permanent";
+      };
     };
   };
 }
