cb060179ae485abd7666f44c05d4b2c3e8a8b442
Daniel Pieper <daniel@daniel-pieper.com>
2026-03-31 16:22:43 +0200

    Configure Filebrowser with Authelia proxy auth via Remote-User header

--- modules/nixos/services/filebrowser/default.nix
+++ modules/nixos/services/filebrowser/default.nix
@@ -22,12 +22,22 @@ in
           address = "127.0.0.1";
           port = 8085;
           root = "/var/lib/filebrowser/data";
+          auth = {
+            method = "proxy";
+            header = "Remote-User";
+          };
         };
       };
 
       caddy.virtualHosts."files.${domain}" = {
         useACMEHost = domain;
-        extraConfig = "reverse_proxy 127.0.0.1:8085";
+        extraConfig = ''
+          forward_auth 127.0.0.1:9091 {
+            uri /api/authz/forward-auth
+            copy_headers Remote-User Remote-Groups Remote-Email Remote-Name
+          }
+          reverse_proxy 127.0.0.1:8085
+        '';
       };
     };
   };
